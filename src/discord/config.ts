cff450ae9f0e23ca576a2039fe177482e1ba5c7b
jbillman <billman377@gmail.com>
2022-01-21 16:43:31 -0700

    added staff role id to config

--- src/discord/config.ts
+++ src/discord/config.ts
@@ -5,5 +5,6 @@ export const options: DiscordBotOptions = {
   intents: new Intents(['GUILDS', 'GUILD_MESSAGES']),
   chatChannelId: '925551422949433374',
   skipChannelId: '928915822209019914',
-  loggingChannelId: '929299105459613716'
+  loggingChannelId: '929299105459613716',
+  staffRoleId: '932470542051704832'
 };
