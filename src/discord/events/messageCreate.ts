24a3eb3b04e4f4dc3f6c4d8f2e08d8508d2f087e
jbillman <billman377@gmail.com>
2022-01-21 20:54:46 -0700

    Added ignoring mentions and replies in mc chat channel

--- src/discord/events/messageCreate.ts
+++ src/discord/events/messageCreate.ts
@@ -1,14 +1,25 @@
-import { ClientEvents } from 'discord.js';
+import { ClientEvents, Message, MessageMentions } from 'discord.js';
+const { USERS_PATTERN, CHANNELS_PATTERN, ROLES_PATTERN, EVERYONE_PATTERN } = MessageMentions;
 import { DiscordClient } from '@customTypes';
 import { options } from '../config';
 import { createEventHandler } from '../../utils/utils';
+
+function shouldIgnore(message: Message): boolean {
+  const hasMention = [USERS_PATTERN, CHANNELS_PATTERN, ROLES_PATTERN, EVERYONE_PATTERN].some(
+    (pattern) => message.content.match(pattern)
+  );
+  const isReply = message.type === 'REPLY';
+
+  return hasMention || isReply;
+}
 
 const eventHandler = createEventHandler<ClientEvents>()({
   event: 'messageCreate',
   handle(message) {
     if (message.channelId !== options.chatChannelId || message.author.bot) return;
+    if (shouldIgnore(message)) return;
     const client = message.client as DiscordClient;
-    const content = `${message.author.username}: ${message.content}`
+    const content = `${message.author.username}: ${message.content}`;
     client.eventBridge.emit('discordMessage', content);
   },
 });
